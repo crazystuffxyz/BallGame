bb798497c9b61f9a7caadb047f8b69aa3ad86662
Crazystuffxyz <125907466+crazystuffxyz@users.noreply.github.com>
2026-08-18 12:01:54 -0400

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -8,8 +8,8 @@ export class Player {
 
         // Base Dimensions & Two Distinct Hitboxes
         this.radius = 0.78;
-        this.hazardRadius = this.radius * 1.1;           // 1.1x larger for obstacle damage (0.859)
-        this.groundRadius = this.hazardRadius * 0.75;    // 0.75x of hazard hitbox for tiles & diagonals (0.6435)
+        this.hazardRadius = this.radius * 1.2;           // 1.1x larger for obstacle damage (0.859)
+        this.groundRadius = this.hazardRadius * 0.3;    // 0.75x of hazard hitbox for tiles & diagonals (0.6435)
 
         this.pos = new THREE.Vector3(0, this.radius, 0);
         this.velY = 0;
